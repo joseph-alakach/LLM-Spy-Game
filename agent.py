d21707c43c8fec66fd02efffb81fdb410ad6ae7c
joseph-alakach <joseph-alakach@edu.aua.am>
2025-03-03 22:12:59 +0400

    initial chatting

--- agent.py
+++ agent.py
@@ -5,14 +5,47 @@ class Agent:
     LLM_NAMES = ["openai"]
     # ROLES = ["spy", "detective"]
 
-    def __init__(self, llm_name: str, player_name: str, player_role: str):
+    def __init__(self, llm_name: str, player_name: str, player_role: str, secret_word: str):
         self.llm_name = llm_name
         self.player_name = player_name
         self.role = player_role
+        self.secret_word = f"The secret_word is : {secret_word}" if secret_word != "" else "You were not given the secret_word"
         self.player_name_conversation_log = []
 
 
-    def ask_question(self, conversation: dict):
+    def ask_question(self, conversation: str) -> str:
+        question = ""
+        try:
+
+            if self.llm_name == self.LLM_NAMES[0]:
+
+                llm_response = config.OPENAI_MODEL.chat.completions.create(
+                    model=prompts_constants.GPT_4O,
+                    messages=[
+                        {"role": "system",
+                         "content": f"{prompts_constants.SYSTEM_PROMPTS.get('rules')} \n{prompts_constants.SYSTEM_PROMPTS.get(self.role)} \nYour name in the game is: {self.player_name} \n{self.secret_word}" },
+                        {"role": "user",
+                         "content": f"This is the conversation record so far {conversation}. \n Now it is your turn to ask a question to the player next to you, ask a question. Return only the question: Your question: "},
+                    ],
+                    temperature=0.3,
+                )
+                question = llm_response.choices[0].message.content
+
+            else:
+                pass
+
+            if question == "":
+                raise Exception
+
+            self.player_name_conversation_log.append(question)
+            return question
+
+        except Exception as e:
+            print(e)
+            return f"Error generating question: llm_name: {self.llm_name}, player_name: {self.player_name}, role: {self.role}"
+
+
+    def respond_to_question(self, conversation: str) -> str:
         response = ""
         try:
 
@@ -21,12 +54,13 @@ class Agent:
                 llm_response = config.OPENAI_MODEL.chat.completions.create(
                     model=prompts_constants.GPT_4O,
                     messages=[
-                        {"role": "system", "content": prompts_constants.SYSTEM_PROMPTS.get(self.role)},
-                        # {"role": "user", "content": f"user prompt: {user_prompt}, page name: {page_name}"},
+                        {"role": "system",
+                         "content": f"{prompts_constants.SYSTEM_PROMPTS.get('rules')} \n{prompts_constants.SYSTEM_PROMPTS.get(self.role)} \nYour name in the game is: {self.player_name} \n{self.secret_word}"},
+                        {"role": "user",
+                         "content": f"This is the conversation record so far {conversation}. \nNow it is your turn to respond to the last question in the conversation record. Return only your response: Your response: "},
                     ],
                     temperature=0.3,
                 )
-
                 response = llm_response.choices[0].message.content
 
             else:
@@ -39,14 +73,39 @@ class Agent:
             return response
 
         except Exception as e:
-            response = (f"{e}"
-                        f"Error generating response: llm_name: {self.llm_name}, player_name: {self.player_name}, role: {self.role}")
+            print(e)
+            return f"Error generating response: llm_name: {self.llm_name}, player_name: {self.player_name}, role: {self.role}"
+
+
+    def vote(self, conversation: str, number_of_players: int) -> int:
+        response = ""
+        list_of_players = [f'player_{i}' for i in range(number_of_players)]
+        try:
+
+            if self.llm_name == self.LLM_NAMES[0]:
+
+                llm_response = config.OPENAI_MODEL.chat.completions.create(
+                    model=prompts_constants.GPT_4O,
+                    messages=[
+                        {"role": "system",
+                         "content": f"{prompts_constants.SYSTEM_PROMPTS.get('rules')} \n{prompts_constants.SYSTEM_PROMPTS.get(self.role)} \nYour name in the game is: {self.player_name} \n{self.secret_word}"},
+                        {"role": "user",
+                         "content": f"This is the conversation record so far {conversation}. \nNow it is the voting round based on the conversation record vote a player. From this list choose only 1 {list_of_players}. Your response: "},
+                    ],
+                    temperature=0.3,
+                )
+                response = llm_response.choices[0].message.content.replace("player_","")
+                response = int(response)
+
+            else:
+                pass
+
+            if response == "":
+                raise Exception
+
+            self.player_name_conversation_log.append(response)
             return response
 
-
-    def respond_to_question(self):
-        pass
-
-
-    def vote(self, player):
-        pass
+        except Exception as e:
+            print(e)
+            return -1
--- game.py
+++ game.py
@@ -1,19 +1,51 @@
 import random
 from typing import List
 from agent import Agent
+import numpy as np
 
 
 class Game:
-    conversation_record = {}
-
+    game_record = {}
     def __init__(self, number_of_players):
-        spy_number = random.randint(1, number_of_players)
-        self.players = [Agent(llm_name="openai", player_name=f"player_{i+1}", player_role="spy") if i+1 == spy_number else Agent(llm_name="openai", player_name=f"player_{i+1}", player_role="detective") for i in range(number_of_players)]
+        self.secret_word = "airplane"
+        self.spy_number = random.randint(0, number_of_players-1)
+        self.players = [Agent(llm_name="openai", player_name=f"player_{i}", player_role="spy", secret_word="") if i == self.spy_number else Agent(llm_name="openai", player_name=f"player_{i}", player_role="detective", secret_word=self.secret_word) for i in range(number_of_players)]
         self.number_of_players = len(self.players)
-        self.spy = random.choice(players)
+        self.game_record["conversation_record"]= "**Game Starts**"
+        self.game_record["voting_record"] = ""
+        self.game_record["player_with_most_votes"] = ""
+        self.game_record["winner"] = ""
+
+
+    def run(self):
+        round = 1
+        player_to_ask_index = 0
+        while round <= 3:
+            player_to_respond_index = (player_to_ask_index + 1) % (7 + 1)
+
+            self.game_record["conversation_record"] +=f"\nplayer_{player_to_ask_index} asks player_{player_to_respond_index} the question: {self.players[player_to_ask_index].ask_question(conversation=self.game_record['conversation_record'])}"
+            self.game_record["conversation_record"] +=f"\nplayer_{player_to_respond_index} responds: {self.players[player_to_respond_index].respond_to_question(conversation=self.game_record['conversation_record'])}"
+
+            round += 1
+            player_to_ask_index = (player_to_ask_index + 1) % (7 + 1)
+        self.game_record["conversation_record"] +=f"\n**End of Conversation Rounds**"
+
+        votes = [ 0 for player in self.players]
+        for player_index, player in enumerate(self.players):
+            player_voted_on = player.vote(self.game_record["conversation_record"], self.number_of_players)
+            votes[player_voted_on] += 1
+            self.game_record["voting_record"] +=f"\nplayer_{player_index} voted on: player_{player_voted_on}"
+
+        player_with_most_votes = np.argmax(votes)
+        self.game_record["player_with_most_votes"] = player_with_most_votes
+
+        if player_with_most_votes == self.spy_number:
+            self.game_record["winner"] = "detectives"
+        else:
+            self.game_record["winner"] = "spy"
 
 
 
-
-
-
+game = Game(7)
+game.run()
+print(game.game_record)
