d0a4f69a19ef16a4c0dc09bfb33077731ba58f71
Aram Leblebjian <aramleblebjian@Arams-MacBook-Pro.local>
2025-04-26 23:38:03 +0400

    .

--- config.py
+++ config.py
@@ -18,7 +18,6 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_CLIENT = genai.Client(api_key=GEMINI_API_KEY)
 GEMINI_MODEL = "gemini-2.5-flash-preview-04-17"
 GEMINI_CONFIG = types.GenerateContentConfig(thinking_config=types.ThinkingConfig(thinking_budget=4096))
-# GEMINI_MODEL = "gemini-2.5-pro-preview-03-25"
 
 # === DeepSeek ===
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
@@ -35,7 +34,6 @@ CLAUDE_MODEL = "claude-3-7-sonnet-20250219"
 GROK_API_KEY = os.getenv("GROK_API_KEY")
 GROK_CLIENT = OpenAI(api_key=GROK_API_KEY, base_url="https://api.x.ai/v1")
 GROK_MODEL = "grok-3-mini-beta"
-# GROK_MODEL = "grok-3-beta"
 
 # === HuggingFace ===
 HUGGINGFACE_MODEL = 'lxyuan/distilbert-base-multilingual-cased-sentiments-student'
